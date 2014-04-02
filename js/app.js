6435e1b1c4736ed802a38f792bae917c158c00c1
Alex Montas <Alexmontasmusic@gmail.com>
2014-04-02 19:11:20 -0400

    Submitting this cool RYU

--- js/app.js
+++ js/app.js
@@ -1,39 +1,48 @@
 $(document).ready(function() {
-
-	 $('.ryu').mouseenter(function() {
-    		$('.ryu-still').hide();
-			$('.ryu-ready').show();
-  });
-  
-  $('.ryu').mouseleave(function() {
-    		$('.ryu-ready').hide();
-			$('.ryu-still').show();
-  });
-
-   $('.ryu').mousedown(function() {
-    	playHadouken ();
-	    $('.ryu-ready').hide();
-	    $('.ryu-throwing').show();
-	    $('.hadouken').show()
-		 .animate(
-		  {'left': '300px'},
-		  500,
-		  function() {
-		    $(this).hide();
-		    $(this).css('left', '-212px');
-		  }
-		
-		);
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();    
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '300px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '-212px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   })
 
-   $('.ryu').mouseup(function() {
-		  $('.ryu-throwing').hide();
-		  $('.ryu-ready').show();
-	});
+  .keydown(function(e) {
+
+  	if(e.keycode== 68){
+  		 $('.ryu-ready').hide();
+  		 $('.ryu-cool').show();
+  	}
+  	else{
+
+  		
+  	}
 
 
-})
+    });
 
+
+});
+  
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
